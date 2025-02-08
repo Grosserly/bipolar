240e64722edb6bb4a084a68b42651a5947127291
Nate Kean <14845347+garlic-os@users.noreply.github.com>
2025-02-08 12:10:59 -0600

    Comment
    Why did alembic format these docstrings this way?

--- parrot/alembic/env.py
+++ parrot/alembic/env.py
@@ -31,14 +31,11 @@ else:
 def run_migrations_offline() -> None:
 	"""Run migrations in 'offline' mode.
 
-	This configures the context with just a URL
-	and not an Engine, though an Engine is acceptable
-	here as well.  By skipping the Engine creation
-	we don't even need a DBAPI to be available.
+	This configures the context with just a URL and not an Engine, though an
+	Engine is acceptable here as well. By skipping the Engine creation we don't
+	even need a DBAPI to be available.
 
-	Calls to context.execute() here emit the given string to the
-	script output.
-
+	Calls to context.execute() here emit the given string to the script output.
 	"""
 	context.configure(
 		url=parrot_cfg.db_url,
@@ -54,9 +51,8 @@ def run_migrations_offline() -> None:
 def run_migrations_online() -> None:
 	"""Run migrations in 'online' mode.
 
-	In this scenario we need to create an Engine
-	and associate a connection with the context.
-
+	In this scenario we need to create an Engine and associate a connection with
+	the context.
 	"""
 	PREFIX = "sqlalchemy."
 	config = alembic_cfg.get_section(alembic_cfg.config_ini_section, {})
